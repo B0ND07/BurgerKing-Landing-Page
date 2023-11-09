1e2fce52d40eefceb6bff0c29a1cce8e63d5c66b
b0nd07 <mhashimkp@gmail.com>
2023-11-10 00:42:36 +0530

    BK: making responsive using grid

--- src/Body.jsx
+++ src/Body.jsx
@@ -3,11 +3,13 @@ import Container from 'react-bootstrap/Container';
 import { MdShoppingBag } from 'react-icons/md';
 import { GiScooter } from 'react-icons/gi';
 import "./App.css"
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
 
 function Body() {
   return (
     <div className='container' style={{marginTop:"2px",display:"flex"}}>
-        <div className='title'>
+        {/* <div className='title'>
             <h1 className='heading-1'>Start your BK<sup>®</sup>order.</h1>
             <div className='button-body'>
             <button className='button1'><MdShoppingBag/> Order Pickup</button><br /><br />
@@ -15,8 +17,25 @@ function Body() {
             </div>
         </div>
         <div className='photo'>
-             <img /*style={{maxWidth:"100%",height:"100%"}}*/ src='https://cdn.sanity.io/images/czqk28jt/prod_bk_us/61dc8eae0e6386cb63746d382a0ed9433fcbbaf4-800x450.jpg?w=650&q=90&fit=max&auto=format'/>
+             <img src='https://cdn.sanity.io/images/czqk28jt/prod_bk_us/61dc8eae0e6386cb63746d382a0ed9433fcbbaf4-800x450.jpg?w=650&q=90&fit=max&auto=format'/>
+        </div> */}
+        <Row xs={1} md={2} lg={2} className="g-4">
+          <Col>
+          <h1 className='heading-1'>Start your BK<sup>®</sup>order.</h1>
+            <div className='button-body'>
+            <button className='button1'><MdShoppingBag/> Order Pickup</button><br /><br />
+            <button className='button2'><GiScooter/> Order Delivery</button>
+            </div>
+
+
+
+          </Col>
+          <Col>
+          <div className='photo'>
+             <img  /*style={{maxWidth:"100%",height:"100%"}}*/  src='https://cdn.sanity.io/images/czqk28jt/prod_bk_us/61dc8eae0e6386cb63746d382a0ed9433fcbbaf4-800x450.jpg?w=650&q=90&fit=max&auto=format'/>
         </div>
+        </Col>
+        </Row>
 
 
     </div>
--- src/Bottom.jsx
+++ src/Bottom.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Col from 'react-bootstrap/esm/Col';
+import Row from 'react-bootstrap/esm/Row';
 import {
     GooglePlayButton,
     AppStoreButton,
@@ -10,7 +12,7 @@ import {
 function Bottom() {
   return (
     <div className='container' style={{paddingTop:"50px",display:"flex"}}>
-        <div className='botttom-image'>
+        {/* <div className='botttom-image'>
             <img src="https://cdn.sanity.io/images/czqk28jt/prod_bk_us/57382e9feb53d08237fa988a570efdff1602d6ff-912x1267.png?w=650&q=80&fit=max&auto=format" width="60%" style={{transform: "rotate(360deg)",marginLeft:"120px"}}></img>
         </div>
 
@@ -21,7 +23,28 @@ function Bottom() {
         <GooglePlayButton theme={"dark"}/>
             </div><br />
             <p>Apple and the Apple logo are trademarks of Apple Inc., registered in the U.S. and other countries. App Store is a service mark of Apple Inc. Google Play is a trademark of Google Inc. Terms apply.</p>
+        </div> */}
+
+      <Row xs={1} md={2} lg={2} className="g-4">
+          <Col>
+          <div className='botttom-image'>
+            <img src="https://cdn.sanity.io/images/czqk28jt/prod_bk_us/57382e9feb53d08237fa988a570efdff1602d6ff-912x1267.png?w=650&q=80&fit=max&auto=format" width="60%" style={{transform: "rotate(360deg)",marginLeft:"120px"}}></img>
         </div>
+          </Col>
+          <Col>
+          <div className='bottom-title'>
+            <h1>Save $$$<br/> With offers on demand</h1>
+            <div style={{display:"flex",gap:"20px"}}>
+        <AppStoreButton theme={"dark"}/> 
+        <GooglePlayButton theme={"dark"}/>
+            </div><br />
+            <p>Apple and the Apple logo are trademarks of Apple Inc., registered in the U.S. and other countries. App Store is a service mark of Apple Inc. Google Play is a trademark of Google Inc. Terms apply.</p>
+        </div>
+
+          
+          
+          </Col>
+      </Row>
 
 
 
